859e22ee72099fd7393bcb37a5205091ad5fc8fc
xushengxing <xusx1024@163.com>
2018-03-09 16:05:55 +0800

    求最大子数组和

---
+++ linklist/MaxSubarray/src/MaxSubarray.java
@@ -0,0 +1,81 @@
+
+public class MaxSubarray {
+    public static void main(String[] args) {
+        MaxSubarray maxSubarray = new MaxSubarray();
+        int[] arr = {1, -2, 3, 10, -4, 7, 2, -5};
+        maxSubarray.fun1(arr);
+        System.out.println("fun2:" + maxSubarray.fun2(arr, 0, arr.length - 1));
+        maxSubarray.fun3(arr);
+        maxSubarray.fun4(arr);
+    }
+
+    void fun1(int[] arr) {//  线性时间法
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum > max)
+                max = sum;
+            if (sum < 0)
+                sum = 0;
+        }
+        System.out.println("fun1:" + max);
+    }
+
+    int fun2(int[] arr, int low, int high) {// 动态规划法
+        if (low >= high) return arr[low];
+        int mid = (high + low) / 2;
+        int lmax = fun2(arr, low, mid);
+        int rmax = fun2(arr, mid + 1, high);
+
+        int mmax = arr[mid];
+        int ans = mmax;
+
+        for (int i = mid - 1; i >= low; i--) {
+            ans += arr[i];
+            mmax = mmax < ans ? ans : mmax;
+        }
+
+        ans = mmax;
+
+        for (int i = mid + 1; i < high; i++) {
+            ans += arr[i];
+            mmax = mmax < ans ? ans : mmax;
+        }
+
+        int t = lmax > rmax ? lmax : rmax;
+        ans = t > mmax ? t : mmax;
+
+        return ans;
+
+    }
+
+    void fun3(int[] arr) {//暴力求解法
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum > max)
+                    max = sum;
+            }
+        }
+        System.out.println("fun3:" + max);
+    }
+
+    void fun4(int[] arr) {// 线性时间法
+        int[] C = new int[arr.length];
+        int max = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (C[i - 1] > 0) {
+                C[i] = C[i - 1] + arr[i];
+            } else {
+                C[i] = arr[i];
+            }
+
+            max = C[i] > max ? C[i] : max;
+        }
+        System.out.println("fun4:" + max);
+    }
+}
+
--- sort/InsertionSort/src/ShellSort.java
+++ sort/InsertionSort/src/ShellSort.java
@@ -1,2 +1,33 @@
+
 public class ShellSort {
+    public static void main(String[] args) {
+        ShellSort ss = new ShellSort();
+        int arr[] = {29, 10, 14, 37, 14};
+        ss.standardSort(arr, 5);
+        for (int i : arr)
+            System.out.print(i + " ");
+    }
+
+
+    // 严格按照定义来实现
+      void standardSort(int[] arr, int n) {
+        int i, j, gap;
+        for (gap = n / 2; gap > 0; gap /= 2) {
+            for (i = 0; i < gap; i++) {
+                for (j = i + gap; j < n; j += gap) {
+                    if (arr[j] < arr[j - gap]) {
+                        int temp = arr[j];
+                        int k = j - gap;
+                        while (k >= 0 && arr[k] > temp) {
+                            arr[k + gap] = arr[k];
+                            k -= gap;
+                        }
+                        arr[k + gap] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+
 }
